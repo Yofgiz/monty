fba9daa55e94cd6c6086fed11d79dd30596bf8bb
Yofgiz <yoftahegizaws@gmail.com>
2023-12-14 06:39:54 -0800

    updated

---
+++ error_1.c
@@ -0,0 +1,64 @@
+
+
+#include "monty.h"
+
+int usage_error(void);
+int malloc_error(void);
+int f_open_error(char *filename);
+int unknown_op_error(char *opcode, unsigned int line_number);
+int no_int_error(unsigned int line_number);
+
+/**
+ * usage_error - Prints usage error messages occured.
+ * Return: (EXIT_FAILURE)
+ */
+int usage_error(void)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	return (EXIT_FAILURE);
+}
+
+/**
+ * malloc_error - Prints malloc error messages always.
+ * Return: (EXIT_FAILURE)
+ */
+int malloc_error(void)
+{
+	fprintf(stderr, "Error: malloc failed\n");
+	return (EXIT_FAILURE);
+}
+
+/**
+ * f_open_error - Prints file opening error messages w/ file name of.
+ * @filename: Name of file failed to open ok
+ * Return: (EXIT_FAILURE)
+ */
+int f_open_error(char *filename)
+{
+	fprintf(stderr, "Error: Can't open file %s\n", filename);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * unknown_op_error - Prints unknown instruction error messages occured.
+ * @opcode: Opcode where error occurred now and then.
+ * @line_number: Line number in Monty file where error occured.
+ * Return: (EXIT_FAILURE)
+ */
+int unknown_op_error(char *opcode, unsigned int line_number)
+{
+	fprintf(stderr, "L%u: unknown instruction %s\n",
+		line_number, opcode);
+	return (EXIT_FAILURE);
+}
+
+/**
+ * no_int_error - Prints invalid monty_push argument error messages to the max.
+ * @line_number: Line number in Monty file where error occurred.
+ * Return: (EXIT_FAILURE) 
+ */
+int no_int_error(unsigned int line_number)
+{
+	fprintf(stderr, "L%u: usage: push integer\n", line_number);
+	return (EXIT_FAILURE);
+}
---
+++ int_to_string.c
@@ -0,0 +1,87 @@
+
+#include <stdlib.h>
+
+char *get_int(int num);
+unsigned int _abs(int);
+int get_numbase_len(unsigned int num, unsigned int base);
+void fill_numbase_buff(unsigned int num, unsigned int base,
+		       char *buff, int buff_size);
+
+/**
+ * get_int - gets a character pointer to new string containing int
+ * Return: character pointer to newly created string. NULL if malloc fails.
+ */
+char *get_int(int num)
+{
+	unsigned int temp;
+	int length = 0;
+	long num_l = 0;
+	char *ret;
+
+	temp = _abs(num);
+	length = get_numbase_len(temp, 10);
+
+	if (num < 0 || num_l < 0)
+		length++; 
+	ret = malloc(length + 1); 
+	if (!ret)
+		return (NULL);
+
+	fill_numbase_buff(temp, 10, ret, length);
+	if (num < 0 || num_l < 0)
+		ret[0] = '-';
+
+	return (ret);
+}
+
+/**
+ * _abs - gets the absolute value of an integer
+ * Return: unsigned integer abs rep of i
+ */
+unsigned int _abs(int i)
+{
+	if (i < 0)
+		return (-(unsigned int)i);
+	return ((unsigned int)i);
+}
+
+/**
+ * get_numbase_len - gets length of buffer needed.
+ * @base: base of number representation used by buffer
+ * Return: integer containing length of buffer needed
+ */
+int get_numbase_len(unsigned int num, unsigned int base)
+{
+	int len = 1; /* all numbers contain atleast one digit */
+
+	while (num > base - 1)
+	{
+		len++;
+		num /= base;
+	}
+	return (len);
+}
+
+/**
+ * fill_numbase_buff - fills buffer with correct numbers up to base 36
+ * @base: base of number used in conversion, only works up to base 36
+ * @buff: buffer to fill with result of conversion
+ * Return: always void.
+ */
+void fill_numbase_buff(unsigned int num, unsigned int base,
+			char *buff, int buff_size)
+{
+	int rem, i = buff_size - 1;
+
+	buff[buff_size] = '\0';
+	while (i >= 0)
+	{
+		rem = num % base;
+		if (rem > 9) /* return lowercase ascii val representation */
+			buff[i] = rem + 87; /* 10 will be a, 11 = b, ... */
+		else
+			buff[i] = rem + '0';
+		num /= base;
+		i--;
+	}
+}
